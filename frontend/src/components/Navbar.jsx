7d6321d3811a2250cbb600c9f51aa8cf4a122aa2
Sathira Sugeesvara <sathirasugeesvara@gmail.com>
2026-07-25 04:00:23 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import './components.css';
+import './Components.css';
 import logo from '../assets/logo.png';
 
 export default function Navbar() {
